c361cea70d7e5765452fa50288172977f732de83
rcastillod <rodrigo.castillo@global3.cl>
2023-04-25 11:19:30 -0400

    feat: :sparkles: Stats section done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+// Data
+import { stats } from "../constants";
+// Styles
+import styles from "../style";
 
-const Stats = () => {
-  return <div>Stats</div>;
-};
+const Stats = () => (
+  <section
+    className={`${styles.flexCenter} flex-col md:flex-row flex-wrap sm:mb-20 gap-5 md:gap-0 mb-6 mt-8 stats`}
+  >
+    {stats.map((stat) => (
+      <div
+        key={stat.id}
+        className={`${styles.flexCenter} flex-col sm:flex-row flex-1 relative`}
+      >
+        <h4 className="font-poppins font-semibold text-white">{stat.value}</h4>
+        <p className="font-poppins font-normal text-gradient uppercase ml-3">
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
 export default Stats;
