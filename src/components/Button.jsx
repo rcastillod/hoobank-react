e5e39a1cee700aaa366a7452671df11293c20842
rcastillod <rodrigo.castillo@global3.cl>
2023-04-25 12:48:45 -0400

    feat: :sparkles: Business section ready

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles, link }) => {
+  return (
+    <a
+      href={link}
+      className={`py-4 px-6 bg-blue-gradient rounded-lg font-medium text-[18px] text-primary ${styles}`}
+    >
+      Get Started
+    </a>
+  );
 };
 
 export default Button;
